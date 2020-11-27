ba73135638f797b695f98c2eaf43c42eff5a593a
Febrian Dwi Putra <febrikimli0@gmail.com>
2020-11-27 18:29:31 +0700

    upload

--- mirror-starter/refresh-mirror.bat
+++ mirror-starter/refresh-mirror.bat
@@ -1,14 +1,19 @@
 @echo off
 SETLOCAL enableDelayedExpansion
+rm y
+rm n
 
-set /P jawab="masuk mode editing (y/n)?"
+set /P jawab="masuk mode editing (y/n)?  :  "
 
 if "%jawab%" == "y" ( 
-	set /P ip="masukan ip android anda (setting->about) : "
+	set /P edit="ingin editing dinyalakan (y/n) : "
+	echo SAVED MODE +=> !edit!
 	rm mode_editing.txt
-	echo SAVED MODE +=> !ip!
-	echo !ip! >> mode_editing.txt
+	echo !edit! >> mode_editing.txt
 )
 
 adb disconnect
 adb devices
+pause
+rm y
+rm n
